f482c3bf94f815d1a3cd27e2bfbb4859e184e2df
Zaqueu Souza <euzaccsouza@gmail.com>
2025-03-31 18:31:19 -0300

    update: src/app.java com adição de exemplos de criação de variaveis

--- 02-TiposDeDadosPrimitivos/src/App.java
+++ 02-TiposDeDadosPrimitivos/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+     /* Inteiros: byte->short->int->long
+        Real:   float -> double
+        Lógico: boolean
+        Caractere: char
+     */
+
+    //Exemplos
+        char inicialNome = 'Z';    
+        int idade = 29;
+        float altura =(float) 1.87; //<-essa atribuição contém o conceito de Casting.
+        boolean eDeMaior = true;
     }
 }
