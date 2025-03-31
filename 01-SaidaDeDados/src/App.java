46d1eeb094eb7988624d69f70f0094ba3e77fdb5
Zaqueu Souza <euzaccsouza@gmail.com>
2025-03-31 17:14:37 -0300

    add: exemplos dos tipos de saídas de dados via console.

--- 01-SaidaDeDados/src/App.java
+++ 01-SaidaDeDados/src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Exemplo de Saída de dados
-        sout
-            
+        System.out.print("1-Imprime uma linha de texto na tela, e se mantém nela. ");
+        System.out.println("2-Imprime o texto na tela e no final da uma quebra de linha.");
+        System.out.printf("3-Imprime o texto na tela e pode fazer formatações no texto.");
     }   
 }
