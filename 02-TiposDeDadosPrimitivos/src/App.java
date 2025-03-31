cb27431f53c974e5d436e9cf752a49ce8b2c4489
Zaqueu Souza <euzaccsouza@gmail.com>
2025-03-31 19:29:18 -0300

    modified: alteração nos exemplos dos tipos de dados primitivos

--- 02-TiposDeDadosPrimitivos/src/App.java
+++ 02-TiposDeDadosPrimitivos/src/App.java
@@ -1,15 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-     /* Inteiros: byte->short->int->long
+     /* Tipos Primitivos no Java
+     
+        Inteiros: byte->short->int->long
         Real:   float -> double
         Lógico: boolean
         Caractere: char
      */
 
     //Exemplos
-        char inicialNome = 'Z';    
-        int idade = 29;
-        float altura =(float) 1.87; //<-essa atribuição contém o conceito de Casting.
-        boolean eDeMaior = true;
-    }
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+        boolean bool;
+        char c;
+    }    
 }
