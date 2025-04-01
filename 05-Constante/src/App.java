d6897de5868be2b137714deae849470c5931dafb
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-01 18:15:03 -0300

    add: adcionado o diretório sobre Constantes

---
+++ 05-Constante/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    // A contante no java é criada dentro do bloco de código da classe
+    public static final float PI = 3.1415f;
+    public static void main(String[] args) throws Exception {
+        System.out.println(PI);
+    }
+}
