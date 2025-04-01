7e1a0a31d346f90df4a4834b33ba7eb7abe8c9ca
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-01 14:47:24 -0300

    modified: alterado o nome do diretorio

--- 04-VariavelFinal/src/App.java
+++ 04-VariavelFinal/src/App.java
@@ -1,17 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        /* Sintaxe da constante:
-            <mofificadorDeAcesso> + <final> + <tipoDaConstante> + <NOME_DA_CONSANTE> 
+         /* Sintaxe da Variavel Final:
+           <final> + <tipoDaConstante> + <NOME_DA_CONSANTE> 
 
-            modificador = modificador de acesso (opcional);
             tipo = tipo de dado primário;
             identificador = nome (identificador) da constante;
             literal = literal que atribui informação à constante.
-        */ 
+        */
 
         // Exemplo
-        final double VALOR_DE_PI=3.1415;
+        final double salarioMinimoAnual = 1518.00f;
 
-        System.out.println(VALOR_DE_PI);
+        System.out.println(salarioMinimoAnual);
     }
 }
