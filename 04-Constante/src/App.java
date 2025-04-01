64bcb8c6e2f9ad356d6423ab62a33bdd4e110f8f
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-01 01:13:47 -0300

    add: diretorio 04-Constante, com exemplos práticos

---
+++ 04-Constante/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /* Sintaxe da constante:
+            <mofificadorDeAcesso> + <final> + <tipoDaConstante> + <NOME_DA_CONSANTE> 
+
+            modificador = modificador de acesso (opcional);
+            tipo = tipo de dado primário;
+            identificador = nome (identificador) da constante;
+            literal = literal que atribui informação à constante.
+        */ 
+
+        // Exemplo
+        final double VALOR_DE_PI=3.1415;
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
