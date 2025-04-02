54a6e6c0cc761c7cc179d2c3ac7b8340e243edd4
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-02 00:39:29 -0300

    add: diretorio contendo exemplos de uso de casting

---
+++ 06-Casting/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // 
+        float altura = (float) 22; // Força a conversão do valor inteiro para um valor com ponto flutuante.
+        int idade = (int) 29.03f; // Força a conversão do valor com ponto flutuante para Inteiro.
+        System.out.println(altura);
+        System.out.println(idade);
+    }
+}
