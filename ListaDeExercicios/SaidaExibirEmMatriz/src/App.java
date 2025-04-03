cafa7616af2e02878d50ee8cae610b2efef6f221
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 02:26:28 -0300

    add: quinto exercicio sobre Saída de Dados via Console

---
+++ ListaDeExercicios/SaidaExibirEmMatriz/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //  Mostre uma tabela de multiplicação de 1 a 10 em formato de matriz.
+        System.out.println("1\t2\t3\t4\t5\t6\t7\t8\t9\t10");
+        System.out.println("2\t4\t6\t8\t10\t12\t14\t16\t18\t20");
+        System.out.println("3\t6\t9\t12\t15\t18\t21\t24\t27\t30");
+        System.out.println("4\t8\t12\t16\t20\t24\t28\t32\t36\t40");
+        System.out.println("5\t10\t15\t20\t25\t30\t35\t40\t45\t50");
+        System.out.println("6\t12\t18\t24\t30\t36\t42\t48\t54\t60");
+        System.out.println("7\t14\t21\t28\t35\t42\t49\t56\t63\t70");
+        System.out.println("8\t16\t24\t32\t40\t48\t56\t64\t72\t80");
+        System.out.println("9\t18\t27\t36\t45\t54\t63\t72\t81\t90");
+        System.out.println("10\t20\t30\t40\t50\t60\t70\t80\t90\t100");
+
+    }
+}
