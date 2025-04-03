e8873da442ac370c60b22b8366ac62de4a8706d5
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-02 22:52:57 -0300

    modified: modificação no nome do diretorio

---
+++ ExercicioHoraSistema/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) {
+        Date dataHora = new Date();
+        System.out.println("A data e hora é: \n" + dataHora.toString());
+    }
+}
