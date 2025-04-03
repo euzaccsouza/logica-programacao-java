5154621074bbd8fc8f488f64e2fde6ed66e9b62e
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 00:28:16 -0300

    add: terceiro exercicio de Saída de Dados via console

---
+++ ListaDeExercicios/SaidaExibirTabela/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Exercicio: Exiba uma tabela com três colunas e cinco linhas usando print e println.
+        System.out.println(" ______________________________________");
+        System.out.print(  "|____________|____________|____________|\n");
+        System.out.print(  "|____________|____________|____________|\n");
+        System.out.print(  "|____________|____________|____________|\n");
+        System.out.print(  "|____________|____________|____________|\n");
+        System.out.println("|____________|____________|____________|");
+       
+    }
+}
