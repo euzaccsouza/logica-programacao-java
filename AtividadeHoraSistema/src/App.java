dcf163b47a93bd8ff88198d06280772c3dc7286a
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-02 22:57:02 -0300

    delete: deletado o diretorio

--- AtividadeHoraSistema/src/App.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class App {
-    public static void main(String[] args) {
-        Date dataHora = new Date();
-        System.out.println("A data e hora é: \n" + dataHora.toString());
-    }
-}
