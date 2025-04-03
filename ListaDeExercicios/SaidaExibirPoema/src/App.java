c38f8af88da6076c5c4bb0f2f770e2a51b52f972
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 00:42:33 -0300

    add: quarto exercicio sobre Saída de Dados via console

---
+++ ListaDeExercicios/SaidaExibirPoema/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Imprima um poema curto com várias linhas usando apenas uma chamada de print.
+        System.out.print("Outrora eu era daqui, e hoje regresso estrangeiro,\nForasteiro do que vejo e ouço, velho de mim.\nJá vi tudo, ainda o que nunca vi, nem o que nunca verei.\nEu reinei no que nunca fui.\n\n'Fernando Pessoa");
+    }
+}
