005b229a605c81e09d871d70f52bb702362c87f2
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-04 01:39:13 -0300

    add: resolução do primeiro exercício sobre variável final

---
+++ ListaDeExercicios/ArmazenarImposto/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Crie uma variável final para armazenar uma taxa de imposto e mostre seu valor.
+        final float icms = 19.5f;
+        System.out.printf("A taxa de imposto é de: %.1f", icms);
+
+    }
+}
