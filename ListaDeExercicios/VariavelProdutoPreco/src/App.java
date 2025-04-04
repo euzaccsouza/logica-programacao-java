1af39842d846a7e2359c9bcfe179c56335f6b021
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-04 00:15:40 -0300

    add: resolução do primeiro exercício sobre variáveis

---
+++ ListaDeExercicios/VariavelProdutoPreco/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Declare variáveis para armazenar o nome de um produto e seu preço.
+        String produto = "Action Figure Naruto Modo Senin";
+        float preco = 345.97f;
+    }
+}
