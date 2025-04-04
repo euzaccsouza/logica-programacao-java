0f9c1cd88f00a94ef2b75d9b2d5fe67544697425
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-04 01:47:23 -0300

    add: resolução do segundo exercício sobre constantes

---
+++ ListaDeExercicios/ConstanteVelocidadeLuz/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static final int VELOCIDADE_LUZ = 299792458;
+    public static void main(String[] args) throws Exception {
+        // Declare uma constante para representar a velocidade da luz e imprima seu valor;
+        System.out.printf("A velocidade da luz é: %d m/s", VELOCIDADE_LUZ);
+    }
+}
