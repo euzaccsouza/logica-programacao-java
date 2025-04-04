10733c11729354d92c5759efc0c76ab66bed6393
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-04 00:46:09 -0300

    add: resolução do terceiro exercício sobre variáveis

---
+++ ListaDeExercicios/ConcatenarString/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Crie três variáveis do tipo String e concatene seus valores em uma quarta variável.
+        String nome = "Zaqueu";
+        String sobrenome = "Souza";
+        String segundoSobrenome = "Jr.";
+        String concatenarStrings;
+
+        concatenarStrings = nome+sobrenome+segundoSobrenome;
+        System.out.println("Nome Completo: " + concatenarStrings); 
+    }
+}
