b39ed2623fc8e1e8c53267a0744004de40f82046
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 23:46:14 -0300

    add: resolução do quarto exercício sobre tipos primitivos

---
+++ ListaDeExercicios/DeclararNumeroGrande/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Declare uma variável long para representar um número muito grande e exiba seu valor.
+        long numeroEnorme = 9223372036854775807l; // Maior valor suportado pelo tipo long.
+
+        System.out.println("Maior valor suportado: "+ numeroEnorme);
+    }
+}
