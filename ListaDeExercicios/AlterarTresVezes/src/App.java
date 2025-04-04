a00bd1298d6898291763ad4225810013d0d346a4
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-04 01:14:56 -0300

    add: resolução do quinto exercício sobre variáveis

---
+++ ListaDeExercicios/AlterarTresVezes/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Altere o valor de uma variável três vezes e mostre as mudanças.
+        double valor = 5700.00;
+        System.out.println("Valor Inicial: " + valor);
+        valor  = 800.00;
+        System.out.println("Valor atual: " + valor);
+        valor = 600.00;
+        System.out.println("Valor atual: " + valor);
+        valor = 150.00;
+        System.out.println("Valor atual: "+ valor );
+
+    }
+}
