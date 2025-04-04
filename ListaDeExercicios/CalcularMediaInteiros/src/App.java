bff11c80f0c532fd2a3589afb7bfa8dc85cf07e5
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 23:29:50 -0300

    add: resolução do segundo exercício de tipos primitivos.

---
+++ ListaDeExercicios/CalcularMediaInteiros/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Calcule e mostre a média de três números inteiros.
+        int primeiroValor = 50;
+        int segundoValor = 30;
+        int terceiroValor = 80;
+        int mediaInteiros = (primeiroValor+segundoValor+terceiroValor) / 2;
+        
+        System.out.println("A Média é: " + mediaInteiros);
+    }
+}
