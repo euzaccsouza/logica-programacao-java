b3916867047dbb8155031985c0b79fc7553c9295
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-03 23:20:09 -0300

    add: resolução do primeiro exercicio sobre tipos primitivos

---
+++ ListaDeExercicios/DeclararVariaveis/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Declare e inicialize variáveis do tipo int, float, double, char e boolean.
+        int idade = 29;
+        float altura = 1.87f;
+        double salario = 5000.00;
+        char inicialNome = 'Z';
+        boolean recebeuSalario = true;
+    }
+}
