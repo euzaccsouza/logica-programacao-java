ef893fe6395e5f2fa3fa24a2e8e2a22f1d37ba3f
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-05 01:10:10 -0300

    add: resolução do primeiro exercício sobre Wrapper

---
+++ ListaDeExercicios/ConversaoParaWrappers/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Converta um valor inteiro para Inteiro e um valor double para Double.
+
+        // Resposta -> Conversão direta dos valores utilizando o conceito de Casting 
+        Integer valorInteiro = (int) 22; //Conversão do valor literal(22) do tipo int, para um Wrapper.
+        Double valorDouble = (double) 245.00;//Conversão do valor literal(245.00) do tipo double, para um Wrapper.  
+        
+    }
+}
