33c9b6fedb857f9582139759bf086b6eb3c6b5b8
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-05 02:33:18 -0300

    add: resolução do segundo exercício sobre Wrapper

---
+++ ListaDeExercicios/MinimoEMaximo/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Utilize métodos da classe Integer para obter o valor mínimo e máximo.
+        int primeiroValor = 1996;
+        int segundoValor = 2025;
+
+        System.out.println(Integer.min(primeiroValor, segundoValor)); // O menor valor será de: 1996
+        System.out.println(Integer.max(primeiroValor, segundoValor)); // O maior valor será de: 2025
+
+
+    }
+}
