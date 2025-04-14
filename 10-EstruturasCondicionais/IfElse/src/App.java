63541d2fe430c20a5a8a0ab532b0e9fa08adac98
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-14 05:04:10 -0300

    add: diretorio com sintaxe de  condicional if-else

---
+++ 10-EstruturasCondicionais/IfElse/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Sintaxe da Estrutura Condicional If/Else
+
+        // Entre os parênteses deverá ir a condição a ser verififada.
+        if (condition) {
+            /*
+             *  Caso a condição esperada se realize, essa parte do código
+             *  será executada. 
+             * 
+             *  OBS: Caso não ele ignora essa parte e pula para a parte de baixo.
+             */
+        }else{
+            /*
+             * Caso a condição esperada não se realize, a parte de cima ignorada 
+             * e será executada essa parte do código.
+             */
+        }
+    }
+}
