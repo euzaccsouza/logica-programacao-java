35396dde90634486caa92c37056b6c706ce2a588
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-14 19:03:15 -0300

    add: diretório com sintaxe de condicional switch-case

---
+++ 10-EstruturasCondicionais/SwitchCase/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //  Sintaxe Etrutura Condicional Switch/Case
+
+        //  Entre os parênteses irá a "condição" a ser verificada.  
+        switch (condititon) {
+            // Caso o valor informado seja o "1" :
+            case 1:               
+                /*
+                 *  O programa entra nesse bloco de codigo e executa o conteúdo dessa parte.
+                 */
+                break;
+            // Caso o valor informado seja o "2" :
+            case 2: 
+                /*
+                 *  O programa entra nesse bloco de código e executa o conteúdo dessa parte.
+                 */
+                break;
+            // E caso o valor informado não seja nenhum dos acima  
+            default:
+                // Executa esse bloco de código por padrão(default)
+        }
+    }
+}
