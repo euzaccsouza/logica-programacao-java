67dda36f7c95550e18d3a854acd6d9ebd785377c
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-14 20:09:56 -0300

    add: sintaxe da estrutura condicional simples IF

---
+++ 10-EstruturasCondicionais/CondicionalSimples-If/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Estrutura Condicional Simples If -> Sintaxe
+
+        /*  
+         *  Entre parênteses estará a condição a ser verificada,
+         *  e se a condição for cumprida executará o bloco de código abaixo;
+         */
+        if(condicao){ 
+            /*
+             * o bloco de código estará aqui.
+             */
+        }
+    }
+}
