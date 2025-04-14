3499fdabbc3ba0cf4279a14d961f7a74d68d44b7
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-13 23:57:30 -0300

    add: diretório sobre operadores

---
+++ 09-Operadores/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /* 
+         * Operadores
+         * 
+         * Operadores Aritméticos
+         *  +   ->  Adição
+         *  -   ->  Subtração
+         *  *   ->  Multiplicação
+         *  /   ->  Divisão
+         *  %   ->  Móulo ou Resto da Divisão
+         * 
+         * Operadores de Atribuição
+         *  =   ->  Atribuição Simples
+         *  +=  ->  Atribuição Aditiva
+         *  -=  ->  Atribuição Subtrativa
+         *  *=  ->  Atribuição Multiplicativa
+         *  /=  ->  Atribuição Divisiva ou de Divisão
+         *  %=  ->  Atribuição de Módulo
+         *  
+        */
+        
+    }
+}
