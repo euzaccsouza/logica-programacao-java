74737c2f2c9f2586c7d1d06c5b833512e8a88b67
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-15 02:10:13 -0300

    add: sintaxe da estrutura de repetição FOR

---
+++ 11-EstruturaDeRepeticao/EstruturaFor/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Estrutura de Repetição FOR -> Sintaxe
+        
+        /*
+         *  A condição de repetição ficará entre parênteses e é dividida em 3 partes, sendo:
+         *  1º -> Criação e inicialização de uma variável, que servirá de valor inicial;
+         *  2º -> Valor inicial e final, sendo o range de repetição da estrutura.
+         *  3º -> O incremento, o i++ tem a finalidade de incrementar mais 1 na variável
+         *        que contém o valor inicial, criando um contador de repetições.
+         */
+        for(int i = 0; i <= 10; i++){
+            /*
+             * Nessa parte ficará o Bloco de Código que se repetirá.
+             */
+        }
+
+        // OBS: O for é geralmente utilizado quando sabemos a quantidade exata de loop.
+
+    }
+}
