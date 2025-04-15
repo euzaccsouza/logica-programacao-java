abc8f5f220c2cd69ca5bad8f92d76223decc3426
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-15 04:43:02 -0300

    add: sintaxe da estrutura de repetição WHILE

---
+++ 11-EstruturaDeRepeticao/EstruturaWhile/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //  Estrutura de Repetição WHILE -> Sintaxe
+        
+        //  Criação e inicialização de uma variável que servirá de contador e valor inicial.
+        int contador = 0;
+        /*
+         *  Dentro dos parênteses ficará a condição para execução do loop(repetição),
+         *  primeiro ele checa a condição e se o resultado da checagem for o esperado 
+         *  ele executa o loop. 
+         */
+        while(contador <= 10){
+
+            /*
+             *  Bloco de código.
+             */
+
+            //  Depois da execução do bloco de código, será incremementado +1 ao valor da variável. 
+            contador++;
+        }
+    }
+}
