6c8af644a27d11423b3381c97437c0b39a1cafef
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-15 23:59:40 -0300

    add: sintaxe array multidirecional

---
+++ 12-Arrays/ArrayMultidirecional/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Array Multidirecional (Matriz) -> Sintaxe
+
+        // Criação da array => <TipoDaArray> + [][] + <NomeDaArray>
+        int[][] numeros;
+
+        // Criação da array, já inicializada com valores.
+        String[][] nomesESobrenomes = {{"Zaqueu", "Michael", "Lebron"},{"Souza","Jordan", "James"}};
+    }
+}
