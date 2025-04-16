36f028ce24658f8dc47e0a15d19bcc776ffbfe5b
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-16 02:08:35 -0300

    add: imprimir todos os números pares de 1 a 100

---
+++ ListaDeExercicios/ImprimirNumerosPares/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Imprima todos os números pares de 1 até 100;
+
+        for(int i = 0; i <= 100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
