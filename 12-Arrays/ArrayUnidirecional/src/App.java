ccb238e79a86f605556675d58a018abab30abb83
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-15 23:52:18 -0300

    add: sintaxe do array unidirecional

---
+++ 12-Arrays/ArrayUnidirecional/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Array Unidirecional (Vetor) -> Sintaxe
+
+        // Criação de um Array => <TipoDoArray> + [] + <NomeDoArray>
+        String[] nomes;
+
+        //Criação de um array declarando o tamanho máximo.
+        int[] idade = new int[3];
+
+        //  Criação de uma array, já inicializado com valores.
+        int[] anoDeNascimento = {1996, 2000, 1957};
+
+    }
+}
