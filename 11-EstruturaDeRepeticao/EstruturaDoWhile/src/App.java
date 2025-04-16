2df7c8db4dc236a69cd88d536cb9c34a3d9ed66b
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-15 23:37:56 -0300

    add: sintaxe estrutura de repetição DO-WHILE

---
+++ 11-EstruturaDeRepeticao/EstruturaDoWhile/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //  Estrutra de Repetição DO-WHILE -> Sintaxe
+        
+        /*
+         *  Executa primeiramente o bloco de código abaixo
+         *  e só depois de executar, é que faz a verificação da condição.
+         */
+        do { 
+            
+            // Bloco de Código
+
+        } while (true); //  Verificação da condição de repetição.
+    }
+}
