fad89a25d83297f88c20dfec374f07880d636403
Zaqueu Souza <euzaccsouza@gmail.com>
2025-04-23 17:06:13 -0300

    add: declarar e exibir valores de um array(vetor)

---
+++ ListaDeExercicios/DeclareArrayEExiba/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Declare um array de 5 números inteiros e imprima todos os valores.
+        int[] numeros = {10, 20, 30, 40, 50};
+        for (int i = 0; i < 5; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+}
